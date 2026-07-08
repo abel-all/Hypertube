58d9addde24d5066c940c8c50f2d43f469ab1d8b
Abdessamad El Allali <a.elallali@navimarsystems.com>
2026-07-08 16:32:07 +0100

    initialize redux

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,25 @@
+"use client"
+
+import { incrementByAmount } from "@/lib/features/counters/countersSlice";
+import { useAppSelector, useAppDispatch } from "@/lib/hooks";
 
 
-export default function Home() {
+export default function HomePage() {
+  const count = useAppSelector(state => state.counter.value);
+  const dispatch = useAppDispatch();
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        Hypertube
-      </main>
-    </div>
-  );
+    <main>
+      Hypertube
+      
+      <div>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded"
+        onClick={() => dispatch(incrementByAmount(20))}
+        >
+          Increment
+        </button>
+        <div>{count}</div>
+      </div>
+    </main>
+  )
 }
